c8fb6aedf5a42c760f3ec1a03f8bc08b5a087328
PrincceMH <pmarinos@unsa.edu.pe>
2023-08-11 07:52:36 -0500

    update

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react"; 
+import { createContext, useState, useEffect, useCallback } from "react"; 
 import { baseUrl, getRequest, postRequest } from "../utils/services";
+
 
 export const ChatContext = createContext();
 
@@ -7,6 +8,33 @@ export const ChatContextProvider = ({children, user}) => {
     const [userChats, setUserChats] = useState(null);
     const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
     const [userChatsError, setUserChatsError] = useState(null);
+    const [potentialChats, setPotentialChats]=useState([]);
+    const [currentChat,setCurrentChat]=useState(null);
+    const [messages,setMessages]=useState(null);
+    const [isMessagesLoading,setIsMessagesLoading]=useState(false);
+    const [messagesError,setMessagesError]=useState(null);
+
+    
+    useEffect(()=>{
+        const getUsers=async()=>{
+            const response=await getRequest(`${baseUrl}/users`);
+            if(response.error){
+                return console.log("Error al obtener usuarios,response",response)
+            }
+            const pChats=response.filter((u)=>{
+                let isChatCreated=false
+                if(user?._id===u._id)return false;
+                if(userChats){
+                    isChatCreated=userChats?.some((chat)=>{
+                        return chat.members[0]===u._id||chat.members[1]===u._id;
+                    });
+                }
+                return !isChatCreated;
+            });
+            setPotentialChats(pChats)
+        };
+        getUsers()
+    },[userChats])
 
     useEffect(() => {
 
@@ -32,10 +60,55 @@ export const ChatContextProvider = ({children, user}) => {
 
     }, [user])
 
+    useEffect(() => {
+
+        const getMessages = async () => {
+            
+
+                setIsMessagesLoading(true);
+
+                setMessagesError(null);
+
+                const response = await getRequest(`${baseUrl}/messages/${currentChat?._id}`)
+                setIsMessagesLoading(false);
+                
+                if(response.error){
+                    return setUserChatsError(response);
+                }
+
+                setUserChats(response);
+            
+        }
+
+        getUserChats();
+
+    }, [user])
+
+
+
+    const updateCurrentChat=useCallback((chat)=>{
+        setCurrentChat(chat);
+    },[]);
+
+
+    const createChat=useCallback(async(fisrtId,secondId)=>{
+        const response=await postRequest(
+            `${baseUrl}/chats`,JSON.stringify({fisrtId,secondId,}
+        )
+
+    );
+    if(response.error){
+        return console.log("Error al Crear chat",response);
+    }
+    setUserChats((prev)=>[...prev,response]);
+},[]);
     return (<ChatContext.Provider value = {{
         userChats,
         isUserChatsLoading,
         userChatsError,
+        potentialChats,
+        createChat,
+        updateCurrentChat,
         }}
     >
         {children}
