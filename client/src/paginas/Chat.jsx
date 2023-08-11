88e3798539ff8baef453d0d9c9152fd20902a407
PrincceMH <pmarinos@unsa.edu.pe>
2023-08-11 07:56:53 -0500

    Updated

--- client/src/paginas/Chat.jsx
+++ client/src/paginas/Chat.jsx
@@ -1,15 +1,38 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
+import PotentialChats from "../componentes/chat/PotentialChats";
+import { Container, Stack } from "react-bootstrap";
+import UserChat from "../componentes/chat/UserChat";
+import { AutorContext } from "../context/AutorContext";
+
+
+
 
 const chat = () => {
+    const {user}=useContext(AutorContext)
+    const {userChats, isUserChatsLoading, updateCurrentChat} = useContext(ChatContext);
+    
 
-    const {userChats,
-        isUserChatsLoading,
-        userChatsError,} = useContext(ChatContext);
-
-        console.log("UserChats", userChats);
-
-    return <>Chat</>;
-}
+    return(
+        <Container>
+            <PotentialChats/>
+            {userChats?.length<1?null:(
+        <Stack direction="horizontal" gap={4} className="align-items-start">
+            <Stack className="messages-box flex-grow-0" gap={3}>
+                {isUserChatsLoading && <p>Cargando Chats...</p>}
+                {userChats.map((chat,index)=>{
+                    return(
+                        <div key={index} onClick={()=>updateCurrentChat(chat)}>
+                            <UserChat chat={chat} user={user}/>
+                        </div>
+                    )
+                })}
+                </Stack>
+            <p>ChatBox</p>
+        </Stack>)}
+        </Container>
+    ); 
+    
+};
  
 export default chat;
--- server/Routes/chatRoute.js
+++ server/Routes/chatRoute.js
@@ -11,4 +11,4 @@ router.post("/", createChat);
 router.get("/:userId", findUserChats);
 router.get("/find/:firstId/:secondId", findChat);
 
-module.exports = router();
+module.exports = router;
